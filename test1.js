1306a12624b8ff311df9e2ae08d139b084caa35b
Ravichandran Jv <ravichandranjv@users.noreply.github.com>
2017-07-01 17:23:07 +0530

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,29 @@
+var jsDiff = require("diff");
+
+var test1={
+Test1:function(drill,user){
+       var str="";
+        var diff = jsDiff.diffWords(drill, user);
+       
+       this.getmissingwords=function(){       
+            diff.forEach(function(part){
+            if(part.removed){
+                str+=part.value;
+            }  
+            });              
+       return str;
+    };
+    var diff1 = jsDiff.diffWords(drill, user);
+    this.getaddedwords=function(){ 
+        str="";       
+            diff1.forEach(function(part){
+            if(part.added){
+                str+=part.value;
+            }  
+            });              
+       return str;
+    };    
+    
+}    
+};
+module.exports.test1=test1;
