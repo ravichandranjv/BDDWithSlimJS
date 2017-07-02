06d878e1e8dfb808167403ecccea0c0def60d6b9
Ravichandran Jv <ravichandranjv@users.noreply.github.com>
2017-07-02 17:35:25 +0530

    Add files via upload

---
+++ src/twoscenarios.js
@@ -0,0 +1,13 @@
+function myApi(){};
+myApi.prototype = {
+twoscenarios:function(drill,user){
+    this.missingwordscenariotest=function(){       
+	return "missing";
+    };
+
+    this.addedwordscenariotest=function(){ 
+       return "added";
+    };    
+}        
+}
+module.exports=new myApi();
